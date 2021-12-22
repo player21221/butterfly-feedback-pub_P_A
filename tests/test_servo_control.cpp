069460a86bf5105744bf1f6e3df4559b9e658ec2
player21221 <i1080p-ti@live.ru>
2021-12-22 16:11:44 +0300

    add functions

--- tests/test_servo_control.cpp
+++ tests/test_servo_control.cpp
@@ -4,6 +4,48 @@
 #include <fstream>
 #include "../src/servo_iface.h"
 #include "../src/filters.h"
+#include namespace std
+
+double heviside(auto t){
+    if t>=1000000
+    {
+        return .09
+    }
+    else
+    {
+        return 0.
+    }
+}
+
+double sumTwoCos(auto t){
+    return .01*cos(3.14*1*.000001*t)+.02*cos(3.14*4*.000001*t)+.06*cos(3.14*16*.000001*t)
+}
+
+double hevisideHalf(auto t,double Mul){
+    return Mul*heviside(t);
+}
+
+double noise(auto t){
+    std::uniform_real_distribution a;
+    return a.operator();;
+}
+
+double execFunc(auto t, int mode){
+    switch mode{
+        case 1 {
+            return heviside(t);
+        }
+        case 2 {
+            return sumTwoCos(t);
+        }
+        case 3 {
+            return hevisideHalf(t, .2);
+        }
+        case 4{
+            return noise(t);
+        }
+    }
+}
 
 
 int feedback_loop(Json::Value const& jscfg)
@@ -27,22 +69,33 @@ int feedback_loop(Json::Value const& jscfg)
         return -1;
     }
 
+    double torquePrev=0.;
+    double torque = 0.;
+    double thetaZero=0.;
+    bool thetaZeroCaptured=False;
     while (!stop)
     {
         theta = 0;
         dtheta = 0;
-        int status = servo->get_state(t, theta, dtheta, true);
+        
+        int status = servo->get_state(t, theta, dtheta, true);  //эта комманда считывает показания датчика
+
         if (status < 0)
         {
             err_msg("received corrupted packet");
             return -1;
         }
-        double torque = 0.;
+        if (!thetaZeroCaptured) {
+            thetaZero=theta;
+            thetaZeroCaptured=True;
+        }
+        torquePrev=torque;
+        torque = 0.;
         torque = clamp(torque, -0.1, 0.1);
-        servo->set_torque(torque);
+        servo->set_torque(torque);    //эта комманда запускает электродвигатель
         sz = snprintf(buf, sizeof(buf), 
-            "t = %ld, theta = %f, dtheta = %f\n", 
-            t, theta, dtheta
+            "t = %ld, theta = %f, dtheta = %f, torque = %f\n", 
+            t, theta, dtheta, torquePrev
         );
         if (sz < 0)
         {
